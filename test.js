b91ef0184b897d4770560cad8d0b630a0c6c5731
Nicholas Lindley <me@thisoneplace.com>
2022-12-14 18:55:01 -0600

    Only support Node >= 14

--- test.js
+++ test.js
@@ -24,8 +24,7 @@ var publicKeyBCompressed = eccrypto.getPublicCompressed(privateKeyB);
 
 describe("Key conversion", function () {
   it("should allow to convert private key to public", function () {
-    // expect(Buffer.isBuffer(publicKey)).to.be.true;
-    console.log(publicKey.toString("hex"));
+    expect(Buffer.isBuffer(publicKey)).to.be.true;
     expect(publicKey.toString("hex")).to.equal(
       "041b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f70beaf8f588b541507fed6a642c5ab42dfdf8120a7f639de5122d47a69a8e8d1"
     );
